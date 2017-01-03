bd004ed0dccb73caa5fc7cb307d053dfe0987a29
Ahmet Türkmen <ahmetturkmen@users.noreply.github.com>
2017-01-03 21:29:58 +0200

    // Basic example of Constructors

---
+++ ExampleCodes/Constructors_E1.java
@@ -0,0 +1,33 @@
+class line2D {
+
+	public point2D p1;
+	private point2D p2;
+
+	public line2D(point2D A, point2D B) {
+	}
+
+	public line2D(double x1, double y1, double x2, double y2) {
+
+		p1 = new point2D(x1, y1);
+		p2 = new point2D(x2, y2);
+
+	}
+
+	public double calcLength() {
+		double result = 0;
+		return result;
+
+	}
+}
+
+public class Constructors_E1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		point2D p1 = new point2D(5.6, 9.3);
+		point2D l1 = new point2D(6.78, 3.21);
+        line 
+	}
+
+}
