8d9e9bf0d0f9542c5857cb5b05bca9e1e6514b98
Ahmet Türkmen <ahmetturkmen@users.noreply.github.com>
2017-01-09 21:52:52 +0200

    Line class to calculate the length

---
+++ Exams/Final/q5/Line.java
@@ -0,0 +1,20 @@
+package com.company.q5;
+
+/**
+ * Created by ahmetturkmen on 1/9/17.
+ */
+public class Line {
+    private Point2d point1;
+    private Point2d point2;
+
+    public Line(Point2d p1, Point2d p2) {
+        point1 = p1;
+        point2 = p2;
+
+    }
+
+    public double calcLength() {
+        double length = Math.sqrt(Math.pow(point1.getX() - point2.getX(), 2) + Math.pow(point1.getY() - point2.getY(), 2));
+        return length;
+    }
+}
